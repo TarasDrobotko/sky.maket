ec348ab881d8cd0b670760db0d355fc2ab7f232f
TarasDrobotko <drobotkot@gmail.com>
2020-02-24 23:09:12 +0200

    add radio buttons for reviews block

--- js/change-images.js
+++ js/change-images.js
@@ -16,7 +16,10 @@ $(document).ready(function() {
       );
     }
 
-    $(".clients-img img:nth-child(1)").attr("src", "images/tablet/microsoft.png");
+    $(".clients-img img:nth-child(1)").attr(
+      "src",
+      "images/tablet/microsoft.png"
+    );
     $(".clients-img img:nth-child(2)").attr("src", "images/tablet/samsung.png");
     $(".clients-img img:nth-child(3)").attr("src", "images/tablet/ibm.png");
     $(".clients-img img:nth-child(4)").attr("src", "images/tablet/google.png");
@@ -35,9 +38,33 @@ $(document).ready(function() {
       );
     }
 
-    $(".clients-img img:nth-child(1)").attr("src", "images/mobile/microsoft.png");
+    $(".clients-img img:nth-child(1)").attr(
+      "src",
+      "images/mobile/microsoft.png"
+    );
     $(".clients-img img:nth-child(2)").attr("src", "images/mobile/samsung.png");
     $(".clients-img img:nth-child(3)").attr("src", "images/mobile/ibm.png");
     $(".clients-img img:nth-child(4)").attr("src", "images/mobile/google.png");
+
+    $("label.radio").click(function(){  
+      $.ajax({  
+           url:"/",  
+           method:"POST",  
+           data:{},  
+           success:function(data){  
+            if($('#tab1').is(':checked')) {
+                $("#r-col-1").css("display", "block");
+                $("#r-col-2").css("display", "none");
+                $("label#tab-2, label#tab-1").css("position", "static");
+              } else if ($('#tab2').is(':checked')){
+                $("#r-col-2").css("display", "block");
+                $("#r-col-1").css("display", "none");
+                $("label#tab-2, label#tab-1").css("position", "relative");
+                $("label#tab-2").css({"top": "572px","left": "145px" });
+                $("label#tab-1").css({"top": "588px", "left": "128px" });
+              }
+           }  
+      });  
+ });  
   }
 });
