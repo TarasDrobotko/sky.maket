dfaade665ca399b33d31abcd423bbade3fef55e3
TarasDrobotko <drobotkot@gmail.com>
2020-02-24 10:28:30 +0200

    add layer for mobile devices and fix layers for tablet and desktop devices

--- js/change-images.js
+++ js/change-images.js
@@ -2,35 +2,42 @@ $(document).ready(function() {
   windowSize = document.documentElement.clientWidth;
 
   if (windowSize <= 1024) {
-    $("#work-process-1").attr("src", "images/tablet/our-working-process-1.png");
-    $("#work-process-2").attr("src", "images/tablet/our-working-process-2.png");
-    $("#work-process-3").attr("src", "images/tablet/our-working-process-3.png");
-    $("#work-process-4").attr("src", "images/tablet/our-working-process-4.png");
+    for (i = 1; i <= 4; i++) {
+      $("#work-process-" + i).attr(
+        "src",
+        "images/tablet/our-working-process-" + i + ".png"
+      );
+    }
 
-    $("#our-works-block img:nth-child(2)").attr(
-      "src",
-      "images/tablet/our-works-1.png"
-    );
-    $("#our-works-block img:nth-child(3)").attr(
-      "src",
-      "images/tablet/our-works-2.png"
-    );
-    $("#our-works-block img:nth-child(4)").attr(
-      "src",
-      "images/tablet/our-works-3.png"
-    );
-    $("#our-works-block img:nth-child(5)").attr(
-      "src",
-      "images/tablet/our-works-4.png"
-    );
+    for (i = 1; i <= 4; i++) {
+      $("#our-works-block img:nth-child(" + (i + 1) + ")").attr(
+        "src",
+        "images/tablet/our-works-" + i + ".png"
+      );
+    }
 
-     $(".clients-img img:nth-child(1)").attr("src", "images/tablet/microsoft.png");
+    $(".clients-img img:nth-child(1)").attr("src", "images/tablet/microsoft.png");
     $(".clients-img img:nth-child(2)").attr("src", "images/tablet/samsung.png");
     $(".clients-img img:nth-child(3)").attr("src", "images/tablet/ibm.png");
     $(".clients-img img:nth-child(4)").attr("src", "images/tablet/google.png");
   }
 
   if (windowSize <= 320) {
-    $("img").attr("src", "mobileFile.png");
+    for (i = 1; i <= 4; i++) {
+      $("#work-process-" + i).attr(
+        "src",
+        "images/mobile/work-process-" + i + ".png"
+      );
+
+      $("#our-works-block img:nth-child(" + (i + 1) + ")").attr(
+        "src",
+        "images/mobile/our-work-" + i + ".png"
+      );
+    }
+
+    $(".clients-img img:nth-child(1)").attr("src", "images/mobile/microsoft.png");
+    $(".clients-img img:nth-child(2)").attr("src", "images/mobile/samsung.png");
+    $(".clients-img img:nth-child(3)").attr("src", "images/mobile/ibm.png");
+    $(".clients-img img:nth-child(4)").attr("src", "images/mobile/google.png");
   }
 });
